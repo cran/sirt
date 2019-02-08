8d6542818a5216e8822c0dd10ab62cba07ec3bf0
Alexander Robitzsch <robitzsch@ipn.uni-kiel.de>
2019-02-08 12:10:03 +0000

    version 3.2-39

--- R/linking_haberman_als_residual_weights.R
+++ R/linking_haberman_als_residual_weights.R
@@ -1,20 +1,68 @@
 ## File Name: linking_haberman_als_residual_weights.R
-## File Version: 0.14
+## File Version: 0.370
 
 
 linking_haberman_als_residual_weights <- function( logaj, logaAt,
-        logaM, cutoff, wgtM0, eps )
+        logaM, cutoff, wgtM0, eps, estimation="OLS", lts_prop=.5 )
 {
     loga_expected <- TAM::tam_outer( x=logaj, y=logaAt, op="+" )
     loga_resid <- logaM - loga_expected
-    wgt_adj <- 1 + 0 * wgtM0
+    NS <- ncol(wgtM0)
+    NI <- nrow(wgtM0)
+    wgt_adj <- matrix(1, nrow=NI, ncol=NS)
     wgtM <- wgtM0
-    if ( cutoff < Inf ){
-        wgt_adj <- ( 1 - ( loga_resid / cutoff )^2 )^2
-        wgt_adj <- 1 * ( abs( loga_resid ) <=cutoff ) * wgt_adj
-        wgtM <- wgtM0 * wgt_adj + eps
+    k_estimate <- FALSE
+    eps_k <- 1e-5
+
+    #*--- settings
+    if (estimation=="BSQ"){
+        k_fac <- 4.685
+        wgt_fn <- linking_haberman_bisquare_weight
     }
-    res <- list(loga_resid=loga_resid, wgt_adj=wgt_adj, wgtM=wgtM )
+    if (estimation=="HUB"){
+        k_fac <- 1.345
+        wgt_fn <- linking_haberman_huber_weight
+    }
+
+    #********************
+    #-- estimation BSQ or HUB
+    if (estimation %in% c("BSQ","HUB") ){
+        if (cutoff==Inf ){
+            k <- k_fac*MAR_normalized(x=loga_resid)
+            k_estimate <- TRUE
+            cutoff <- k + eps_k
+        }
+        if (cutoff<Inf){
+            min_x <- apply(abs(loga_resid), 2, min, na.rm=TRUE)
+            min_x <- 2*max(min_x)
+            cutoff <- max(cutoff, min_x)
+            args <- list(x=loga_resid, cutoff=cutoff)
+            wgt_adj <- do.call(wgt_fn, args=args )
+        }
+    }
+    #-- estimation LTS
+    if (estimation=="LTS"){
+        for (ss in 1:NS){
+            e <- loga_resid[,ss]
+            e <- e - median(e, na.rm=TRUE)
+            dfr_resid <- data.frame(item=1:NI, e=e )
+            dfr_resid <- na.omit(dfr_resid)
+            dfr_resid <- dfr_resid[ order(abs(dfr_resid$e), decreasing=TRUE), ]
+            wgt_adj[ is.na(loga_resid[,ss]), ss ] <- 0
+            n <- nrow(dfr_resid)
+            n_del <- floor( (1-lts_prop)*n)
+            m1 <- dfr_resid[ 1:n_del, c(2,1) ]
+            wgt_adj[ dfr_resid[ 1:n_del, 1 ], ss ] <- 0
+        }
+    }
+
+    #*** update weights
+    wgt_adj[ is.na(wgt_adj) ] <- 0
+    wgtM <- wgtM0 * wgt_adj + eps
+
+    #--- output
+    res <- list(loga_resid=loga_resid, wgt_adj=wgt_adj, wgtM=wgtM,
+                    cutoff=cutoff, k_estimate=k_estimate)
     return(res)
 }
 
--- R/noharm_sirt_preproc.R
+++ R/noharm_sirt_preproc.R
@@ -1,40 +1,66 @@
 ## File Name: noharm_sirt_preproc.R
-## File Version: 0.31
+## File Version: 0.398
 
 
 #**** data preprocessing noharm.sirt
-noharm_sirt_preproc <- function( dat, weights, Fpatt, Fval,
-    Ppatt, Pval, Psipatt, Psival, wgtm, dimensions )
+noharm_sirt_preproc <- function( dat, pm, N, weights, Fpatt, Fval,
+    Ppatt, Pval, Psipatt, Psival, wgtm, dimensions, pos.loading,
+    pos.variance, pos.residcorr, input_pm, lower, upper )
 {
 
     res <- NULL
     res$dat0 <- dat
 
-    #*****************
-    # data processing
-    res$N <- N <- nrow(dat)
-    res$I <- I <- ncol(dat)
-    if (is.null(weights)){
-        weights <- rep(1,nrow(dat) )
+    if ( ! input_pm ){
+        N <- nrow(dat)
+        I <- ncol(dat)
+        items <- colnames(dat)
+    } else {
+        I <- ncol(pm)
+        items <- colnames(pm)
+        pm <- as.matrix(pm)
     }
-    res$weights <- weights
-    dat.resp <- 1-is.na(dat)
-    dat0 <- dat
-    dat[ dat.resp==0] <- 0
+
+    if (length(lower)==1){
+        lower <- rep(lower, I)
+    }
+    if (length(upper)==1){
+        upper <- rep(upper, I)
+    }
+
+    #---- data processing
+    res$N <- N
+    res$I <- I
+
+    if (! input_pm){
+        if (is.null(weights)){
+            weights <- rep(1, N)
+        }
+        res$weights <- weights
+        dat.resp <- 1-is.na(dat)
+        dat0 <- dat
+        dat[dat.resp==0] <- 0
+        # calculate (weighted) product moment correlation
+        ss <- as.matrix( crossprod( weights*dat.resp ) )
+        eps <- 1e-6
+        pm <- as.matrix( crossprod( as.matrix(dat*weights*dat.resp) ) / ( ss+eps ) )
+    } else {
+        dat.resp <- NULL
+        ss <- 1
+    }
     res$dat <- dat
     res$dat.resp <- dat.resp
-    # calculate (weighted) product moment correlation
-    ss <- as.matrix( crossprod( weights*dat.resp ) )
-    eps <- 1e-6
-    pm <- as.matrix( crossprod( as.matrix(dat*weights*dat.resp) ) / ( ss+eps ) )
     res$pm <- pm
     res$pm0 <- pm
     res$ss <- ss
+    res$lower <- lower
+    res$upper <- upper
+
     # CFA or EFA?
     if ( is.null(dimensions) ){
         model.type <- "CFA"
         modtype <- 3    # 3 - multidimensional CFA
-            } else {
+    } else {
         model.type <- "EFA"
         modtype <- 2    # 2 - multidimensional EFA
         D <- dimensions
@@ -70,8 +96,8 @@ noharm_sirt_preproc <- function( dat, weights, Fpatt, Fval,
     wgtm <- wgtm * ( ss > 0 )
     res$wgtm <- wgtm
     res$sumwgtm <- ( sum( wgtm > 0 ) - sum( diag(wgtm) > 0 ) ) / 2
-    #***
-    # column names
+
+    #*** column names
     D <- ncol(Ppatt)
     cn <- paste0("F",1:D)
     if (is.null(colnames(Fpatt) ) ){
@@ -88,6 +114,79 @@ noharm_sirt_preproc <- function( dat, weights, Fpatt, Fval,
         colnames(Pval) <- colnames(Fval)
     }
     rownames(Pval) <- colnames(Pval)
+
+    #--- create parameter table
+
+    # F
+    parm_table <- noharm_sirt_preproc_parameter_table_matrix(pattmat=Fpatt, valmat=Fval,
+            patt_id=1, patt_label="F", minval=0, symm=FALSE)
+    # P
+    parm1 <- noharm_sirt_preproc_parameter_table_matrix(pattmat=Ppatt, valmat=Pval,
+            patt_id=2, patt_label="P", minval=max(parm_table$index, na.rm=TRUE), symm=TRUE)
+    parm_table <- rbind(parm_table, parm1)
+    # Psi
+    parm1 <- noharm_sirt_preproc_parameter_table_matrix(pattmat=Psipatt, valmat=Psival,
+            patt_id=3, patt_label="Psi", minval=max(parm_table$index, na.rm=TRUE), symm=TRUE)
+    parm_table <- rbind(parm_table, parm1)
+    parm_table <- parm_table[ parm_table$nonnull_par==1, ]
+    rownames(parm_table) <- NULL
+    npar <- max(parm_table$index, na.rm=TRUE)
+
+    # indices
+    ip <- parm_table$index
+    ip[duplicated(ip)] <- NA
+    extract_index <- match(1:npar, ip)
+    extract_index <- intersect( which( ! is.na( parm_table$index ) ),
+                            which( ! duplicated( parm_table$index ) ) )
+    parm_table$est <- parm_table$starts
+
+    parm_table$lower <- -Inf
+
+    ind <- NULL
+    if (pos.variance){
+        ind1 <- which((parm_table$mat=="P") & (parm_table$row==parm_table$col ))
+        ind <- union(ind, ind1)
+    }
+    if (pos.loading){
+        ind1 <- which((parm_table$mat=="F") )
+        ind <- union(ind, ind1)
+    }
+    if (pos.residcorr){
+        ind1 <- which((parm_table$mat=="Psi") )
+        ind <- union(ind, ind1)
+    }
+    parm_table[ind, "lower"] <- 0
+    parm_table[ is.na(parm_table$index), "lower"] <- NA
+
+    non_fixed <- ! parm_table$fixed
+    include_index <- parm_table$index[ non_fixed ]
+    parm_table$nonnull_par <- NULL
+    attr(parm_table, "extract_index") <- extract_index
+    attr(parm_table, "non_fixed") <- non_fixed
+    attr(parm_table, "include_index") <- include_index
+    attr(parm_table, "npar") <- npar
+    attr(parm_table, "NH") <- sum(non_fixed)
+    attr(parm_table, "est_par_index") <- which(parm_table$est_par==1)
+    attr(parm_table, "parm_table_free_index") <- which(parm_table$fixed==0)
+
+    parm_index <- list()
+    for (mat_label in c("F", "P", "Psi") ){
+        ind_mat <- which(parm_table$mat==mat_label)
+        parm_index[[ mat_label ]] <- list()
+        parm_index[[ mat_label ]][[ "row_parm_table" ]] <- ind_mat
+        parm_index[[ mat_label ]][[ "entries" ]] <- parm_table[ ind_mat, c("row","col")]
+        parm_index[[ mat_label ]][[ "len" ]] <- length(ind_mat)
+        nrow <- I
+        ncol <- D
+        if (mat_label=="P"){ nrow <- D}
+        if (mat_label=="Psi"){ ncol <- I}
+        parm_index[[ mat_label ]][["nrow"]] <- nrow
+        parm_index[[ mat_label ]][["ncol"]] <- ncol
+    }
+
+    res$parm_table <- parm_table
+    res$npar <- npar
+    res$parm_index <- parm_index
 
     #*****
     # matrix conversion
@@ -106,7 +205,7 @@ noharm_sirt_preproc <- function( dat, weights, Fpatt, Fval,
     res$Psipatt <- 1*(res$Psipatt>0)
     res$wgtm.default <- wgtm.default
     res$F_dimnames <- colnames(Fpatt)
-    res$items <- colnames(dat)
+    res$items <- items
     return(res)
 }
 
