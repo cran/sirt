e37273acf00d44f026f3f441c634bb7e965aabb4
Alexander Robitzsch <robitzsch@ipn.uni-kiel.de>
2019-03-18 09:50:02 +0000

    version 3.3-26

--- R/invariance_alignment_cfa_config.R
+++ R/invariance_alignment_cfa_config.R
@@ -1,9 +1,10 @@
 ## File Name: invariance_alignment_cfa_config.R
-## File Version: 0.12
+## File Version: 0.221
 
 
-invariance_alignment_cfa_config <- function(dat, group, ...)
+invariance_alignment_cfa_config <- function(dat, group, weights=NULL, ...)
 {
+    CALL <- match.call()
     groups <- unique(group)
     G <- length(groups)
     I <- ncol(dat)
@@ -15,12 +16,17 @@ invariance_alignment_cfa_config <- function(dat, group, ...)
     colnames(nu) <- items
     lambda <- nu
     err_var <- nu
+    weights_gg <- NULL
     for (gg in 1:G){
         dat_gg <- dat[ group==groups[gg], ]
         dat_gg <- dat_gg[, colMeans(is.na(dat_gg)) < 1 ]
         items_gg <- colnames(dat_gg)
         ind_gg <- match(items_gg, items)
-        res <- invariance_alignment_cfa_config_estimate(dat_gg=dat_gg, ...)
+        if (!is.null(weights)){
+            weights_gg <- weights[group==groups[gg]]
+        }
+        res <- invariance_alignment_cfa_config_estimate(dat_gg=dat_gg,
+                        weights_gg=weights_gg, ...)
         nu[gg, ind_gg] <- res$nu
         lambda[gg, ind_gg] <- res$lambda
         err_var[gg, ind_gg] <- res$err_var
@@ -28,6 +34,6 @@ invariance_alignment_cfa_config <- function(dat, group, ...)
     }
     #-- output
     res <- list(nu=nu, lambda=lambda, err_var=err_var, N=N, G=G, I=I,
-                    items=items, groups=groups)
+                    items=items, groups=groups, CALL=CALL)
     return(res)
 }
---
+++ R/lsem_define_lavaan_est_fun.R
@@ -0,0 +1,19 @@
+## File Name: lsem_define_lavaan_est_fun.R
+## File Version: 0.06
+
+lsem_define_lavaan_est_fun <- function(lavaan_fct)
+{
+    if (lavaan_fct=="sem"){
+        lavaan_est_fun <- lavaan::sem
+    }
+    if (lavaan_fct=="lavaan"){
+        lavaan_est_fun <- lavaan::lavaan
+    }
+    if (lavaan_fct=="cfa"){
+        lavaan_est_fun <- lavaan::cfa
+    }
+    if (lavaan_fct=="growth"){
+        lavaan_est_fun <- lavaan::growth
+    }
+    return(lavaan_est_fun)
+}
